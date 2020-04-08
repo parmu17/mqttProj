eb8dd28309cdef48dc6a53ad433cddb243122085
Parmeshwar Kumar Sahu <parmeshwarsahu17@gmail.com>
2020-04-08 18:19:29 +0530

    main file modified for message log distinguish

--- app.js
+++ app.js
@@ -1,23 +1,26 @@
 var mqtt = require('mqtt');
-var client = mqtt.connect({hostname: 'lwptech.co.in', port: 8883, username: 'parmu17', password: 'Parm@18001800', protocol: 'mqtts', capath: '/etc/ssl/certs/'});
+var client = mqtt.connect({hostname: 'lwptech.co.in', port: 8883, username: 'parmu17', password: 'Parm@18001800', capath: '/etc/ssl/certs/', protocol: 'mqtts'});
+
+client.on('error', (err)=>{
+	console.log(err);
+	client.end();
+});
 
 client.on('connect', ()=>{
-	client.subscribe('lwp/brd2/esp1', (err)=>{
+	client.subscribe('lwp/brd1/esp1', (err)=>{
 		if(err){
 			debug.log(err);
 		}
 		if(!err){
+			console.log('Successfully subscribed the topic "lwp/brdq/esp1" ..');
 			client.publish('lwp/brd2/esp1', 'Hello mqtt how are you.. Jai maa Saraswati');
 		}
 	});
 });
 
+//For both subscribed(incoming) and published (outgoing) message
 client.on('message', (topic, msg)=>{
 	console.log(msg.toString());
-	console.log(topic.toString());
+	//console.log(topic.toString());
 	client.end();
 });
-
-
-
-//console.log("Ae mere humsafar ae mre jaaneman..");
